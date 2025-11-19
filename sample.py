7f6766e3aeec3c801d76f7f6a90efe880dc586bf
cmodel1 <cmodel@me.com>
2025-11-19 11:46:16 +0100

    playing around with pydriller

--- sample.py
+++ sample.py
@@ -1,4 +1,7 @@
 import pydriller
 
+cnt = 0
 for commit in pydriller.Repository('transformers/').traverse_commits():
-    print('Hash {}, author {}'.format(commit.hash, commit.author.name))
+    cnt += 1
+    print('Hash {}, author {}, commiter date {}'.format(commit.hash, commit.author.name, commit.committer_date))
+print(f'Iterated over {cnt} commits total.')
